0d1950ba1ba7ac2a82de686c3927d91b1bd3002f
Suresh Thapa <sureshthapa6063@gmail.com>
2026-02-28 07:43:52 +0545

    feat: add JWT protection and admin-only access control to category and product routes

--- backend/src/routes/category.route.js
+++ backend/src/routes/category.route.js
@@ -9,10 +9,16 @@ import {
 import { zodValidate } from "../middlewares/zodValidate.js"
 import upload from "../middlewares/upload.js"
 import { createCategorySchema, updateCategorySchema } from "../validators/category.schema.js"
+import protect from "../middlewares/jwtVerify.js"
+import adminOnly from "../middlewares/adminVerify.js"
 
 const router = express.Router()
 
-router.route("/").get(getCategories).post(
+router.route("/")
+.get(getCategories)
+.post(
+  protect,
+  adminOnly,
   upload.single("image"), // multer file upload middleware
   zodValidate(createCategorySchema), // zod validation middleware
   createCategory
@@ -23,10 +29,12 @@ router
   .route("/:id")
   .get(getCategory)
   .patch(
+     protect,
+     adminOnly,
     upload.single("image"),
     zodValidate(updateCategorySchema), 
     updateCategory
   )
-  .delete(deleteCategory)
+  .delete( protect,adminOnly,deleteCategory)
 
 export default router
--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -3,6 +3,8 @@ import { createProduct, deleteProduct, getProduct, getProducts, updateProduct } 
 import upload from "../middlewares/upload.js"
 import { zodValidate } from "../middlewares/zodValidate.js"
 import { createProductSchema, updateProductSchema } from "../validators/product.schema.js"
+import protect from "../middlewares/jwtVerify.js"
+import adminOnly from "../middlewares/adminVerify.js"
 
 const router = express.Router()
 
@@ -10,6 +12,8 @@ router
 .route("/")
 .get(getProducts)
 .post(
+    protect,
+    adminOnly,
     upload.array('images'),
     // zodValidate(createProductSchema),
     createProduct
@@ -18,8 +22,10 @@ router
 router
 .route('/:slug')
 .get(getProduct)
-.delete(deleteProduct)
+.delete( protect,adminOnly,deleteProduct)
 .patch(
+      protect,
+      adminOnly,
      upload.array('images'),
      zodValidate(updateProductSchema),
      updateProduct
