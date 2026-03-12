34bb098fe2001555e4c91676b6a5fba8eba003c9
Suresh Thapa <sureshthapa6063@gmail.com>
2026-03-12 12:42:40 +0545

    feat: update CartPage to use user cart data and handle empty cart state

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -3,12 +3,17 @@ import { Button } from "../components/ui/button";
 import OrderSummaryCard from "../components/OrderSummaryCard";
 import { useProducts } from "../api/productServices";
 import CartCard from "../components/CartCard";
+import { useUserCart } from "../api/cartServices";
 
 const CartPage = () => {
-    const {data:carts,isPending,error} = useProducts({limit:2})
+    const {data:cart,isPending,error} = useUserCart()
 
     if(isPending) return <p>loading...</p>
     if(error) return <p>{error.message}</p>
+
+    if(!cart || cart.items.length === 0 ) {
+      return <p>Cart is empty!</p>
+    }
     
   return (
     <MaxWidthContainer className="my-16">
@@ -16,13 +21,13 @@ const CartPage = () => {
             Shopping Cart
         </h1>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-12">
-        <div className="col-span-2 space-y-4">
+        {/* <div className="col-span-2 space-y-4">
             {
                 carts.map(item => (
                     <CartCard key={item.id} item={item} />
                 ))
             }
-        </div>
+        </div> */}
 
         <OrderSummaryCard />
       </div>
