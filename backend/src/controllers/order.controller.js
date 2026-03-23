803de9fff97bc19071fb748da72a2b362bcd402a
Suresh Thapa <dursikshya.suresh@gmail.com>
2026-03-23 12:27:57 +0545

    refactor getOrders function

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -2,6 +2,7 @@ import Order from "../models/order.model.js";
 import ErrorMessage from "../utils/ErrorMessage.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import { getPagination } from "../utils/pagination.js";
+import Cart from "../models/cart.model.js";
 
 
 /**
@@ -13,7 +14,10 @@ import { getPagination } from "../utils/pagination.js";
 export const getOrders = asyncHandler(async (req, res, next) => {
   const { limit = 10, page = 1 } = req.query
 
-  const totalOrders = await Order.countDocuments()
+  const totalOrders =
+  req.user.role === "admin"
+    ? await Order.countDocuments()
+    : await Order.countDocuments({ user: req.user.userId })
 
   const pagination = getPagination(page, limit, totalOrders)
   
@@ -32,13 +36,9 @@ export const getOrders = asyncHandler(async (req, res, next) => {
     .skip(pagination.skip)
   }
 
-
-  if(!orders || orders.length === 0) 
-    return next(ErrorMessage("Orders not found",404))  
-
   res.status(200).json({
     success: true,
-    orders,
+    orders: orders || [],
     pagination 
   })
 })
@@ -50,14 +50,24 @@ export const getOrders = asyncHandler(async (req, res, next) => {
  */
 
 export const createOrder = asyncHandler(async (req, res, next) => {
+
+  let cart = await Cart.findOne({ user: req.user.userId })
+      if (!cart) {
+          return next(ErrorMessage("Cart not found!", 404))
+      }
+
   // create order
   const order = await Order.create({
-    user: req.user.userId,
+    user: req.user?.userId,
     ...req.body
   })
 
   // calculate total
   const total = order.items.reduce((acc,item) => acc + item.price * item.quantity, 0 )
+   
+   // clear cart after order is sucessfull
+   cart.items = []
+   await cart.save()
 
   // cash on delivery
   if(order.paymentMethod === "cod"){
